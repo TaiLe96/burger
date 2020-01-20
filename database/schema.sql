f885fd1b1c33670af5562b1122a2860e443d88d4
Tai Le <tle449@gmail.com>
2020-01-20 15:54:33 -0800

    created database and added schema.sql and seed.sql

---
+++ database/schema.sql
@@ -0,0 +1,16 @@
+-- Create the burgers_db.
+-- Switch to or use the burgers_db.
+-- Create a burgers table with these fields:
+-- id: an auto incrementing int that serves as the primary key.
+-- burger_name: a string.
+-- devoured: a boolean.
+
+drop database if exists burgers_db;
+create database burgers_db;
+use burgers_db;
+
+create table burgers (
+    id int not null auto_increment primary key,
+    burger_name varchar(50) not null,
+    devoured boolean default false
+)
---
+++ database/seed.sql
@@ -0,0 +1,8 @@
+-- Still in the db folder, create a seeds.sql file. 
+-- In this file, write insert queries to populate the burgers table with at least three entries
+
+
+insert burgers (burger_name, devoured) values ("Hamburger", true);
+insert burgers (burger_name, devoured) values ("Bacon Burger", true);
+insert burgers (burger_name, devoured) values ("Chicken Burger", true);
+insert burgers (burger_name, devoured) values ("Vegan Burger", true);
